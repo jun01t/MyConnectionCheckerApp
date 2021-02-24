138407fa8e13772e6ad77ca22486f5a21df2a016
TaharaNoNeko <tmdjnch0901@gmail.com>
2021-02-24 19:42:29 +0900

    first commit

--- MyWifiConnectionCheckerApp/ContentView.swift
+++ MyWifiConnectionCheckerApp/ContentView.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
+import SystemConfiguration
+
 
 struct ContentView: View {
+    @State var statusText = "Wi-Fi Check!"
     var body: some View {
-        Text("Hello, world!")
+        Text(statusText)
+            .font(.largeTitle)
             .padding()
+        
+        VStack {
+            Button(action: {
+                if(connectedToNetwork()){
+                    statusText = "connected!"
+                }else{
+                    statusText = "disconnected!"
+                }
+            }) {
+                Text("Check!")
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
+}
+
+func connectedToNetwork() -> Bool {
+    
+    var zeroAddress = sockaddr_in()
+    zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+            SCNetworkReachabilityCreateWithAddress(nil, $0)
+        }
+    }) else {
+        return false
+    }
+    
+    var flags: SCNetworkReachabilityFlags = []
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+        return false
+    }
+    
+    let isReachable = flags.contains(.reachable)
+    let needsConnection = flags.contains(.connectionRequired)
+    
+    return (isReachable && !needsConnection)
 }
 
 struct ContentView_Previews: PreviewProvider {
